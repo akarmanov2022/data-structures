5cd064a77fbc547f44404fdf748bf78796420955
akarmanoff <arseniy.karmanov@mail.ru>
2022-11-15 17:23:32 +0700

    Добавил класс для реализации сортировки Merge Sort.

---
+++ lab2/Sort/MergeSort.cpp
@@ -0,0 +1,19 @@
+//
+// Created by akarmanov on 15-11-2022.
+//
+
+#include "MergeSort.h"
+
+void MergeSort::Merge(List *list)
+{
+
+}
+
+void MergeSort::Sort(List *list)
+{
+    if (list->GetLength() < 2)
+    {
+        return;
+    }
+
+}
---
+++ lab2/Sort/MergeSort.h
@@ -0,0 +1,13 @@
+#include "../List.h"
+
+//
+// Created by akarmanov on 15-11-2022.
+//
+class MergeSort
+{
+private:
+    void Merge(List *list);
+
+public:
+    void Sort(List *list);
+};
