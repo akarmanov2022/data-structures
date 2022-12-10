6527994b4428d85ebf1c560313e0edce3526ac91
akarmanoff <arseniy.karmanov@mail.ru>
2022-12-10 10:54:20 +0700

    Исправления в функции ввода.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,31 @@
 #include <iostream>
 #include <limits>
 
-int InputInt(const char *string);
+int InputInt(const char *string)
+{
+    std::string input;
+    bool valid;
+    while (true)
+    {
+        valid = true;
+        std::cout << string;
+        std::cin >> input;
+        for (char c : input)
+        {
+            if (c < '0' || c > '9')
+            {
+                valid = false;
+                break;
+            }
+        }
+        if (valid)
+        {
+            break;
+        }
+        std::cout << "Invalid input" << std::endl;
+    }
+    return std::stoi(input);
+}
 
 int main()
 {
@@ -82,30 +106,4 @@ int main()
             }
         }
     }
-}
-
-int InputInt(const char *string)
-{
-    std::string input;
-    bool valid;
-    while (true)
-    {
-        valid = true;
-        std::cout << string;
-        std::cin >> input;
-        for (char c : input)
-        {
-            if (c < '0' || c > '9')
-            {
-                valid = false;
-                break;
-            }
-        }
-        if (valid)
-        {
-            break;
-        }
-        std::cout << "Invalid input" << std::endl;
-    }
-    return std::stoi(input);
 }
